54bb39c280a8a2e4343f1b146f582f20c5a140bb
YeahBoi321 <81260626+YeahBoi321@users.noreply.github.com>
2022-02-24 11:21:50 -0500

    Update fileSystem.lua

--- computercraft/123yeah_boi321/fileSystemManagment/fileSystem.lua
+++ computercraft/123yeah_boi321/fileSystemManagment/fileSystem.lua
@@ -1,4 +1,13 @@
-local fsm = require("fsmanager")
+if fs.exists("fsmanager.lua") then
+	local fsm = require("fsmanager")
+else
+	print("Hmm.. It seems you do not have the main file. Would you like me to download it?")
+	print("Y/N")
+	local re = read()
+	if re =="y" or re =="yes" or re == "HEK YEA BROTHER" then
+		shell.run("wget https://raw.githubusercontent.com/Dead-Knuckle/Minecraft-Computer-Mod-Codes/main/computercraft/123yeah_boi321/fileSystemManagment/fsmanager.lua")
+	end
+end
 local dB,dT,dF,dS =fsm.main(false,"","",0)
 term.clear
 while true do
