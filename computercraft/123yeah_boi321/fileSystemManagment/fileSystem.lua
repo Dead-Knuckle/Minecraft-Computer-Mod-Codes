1262b9226058c11dc88a9c8a8b92f14b745a0555
YeahBoi321 <81260626+YeahBoi321@users.noreply.github.com>
2022-03-21 11:39:25 -0400

    Update fileSystem.lua
    removed the use of wget

--- computercraft/123yeah_boi321/fileSystemManagment/fileSystem.lua
+++ computercraft/123yeah_boi321/fileSystemManagment/fileSystem.lua
@@ -1,42 +1,45 @@
 if shell.resolveProgram("fsmanager") then
-	shell.run("wget https://raw.githubusercontent.com/Dead-Knuckle/Minecraft-Computer-Mod-Codes/main/computercraft/123yeah_boi321/fileSystemManagment/fsmanager.lua temp.lua")
-	local tempFile = fs.open("temp.lua", "r")
-	local temp = tempFile.readAll()
-	tempFile.close()
-	local fsmanResolved = shell.resolveProgram("fsmanager")
-	local fsmanFile = fs.open(fsmanResolved, "r")
-	local fsman = fsmanFile.readAll()
-	fsmanFile.close()
-	if fsman ~= temp then
-		print("It seems that fsmanager may need an update. Would you like to install?")
-		print("Y/N")
-		local re = read()
-		if string.lower(re) == "y" or string.lower(re) == "yes" or string.lower(re) == "update" or string.upper(re) == "HEC YEA BROTHER" then
-			fs.delete(fsmanResolved)
-			fs.move("temp.lua",fsmanResolved)
-		end
-	else
-		fs.delete("temp.lua")
-	end
-	term.clear()
+    local temp = http.get("https://raw.githubusercontent.com/YeahBoi321/Minecraft-Computer-Mod-Codes/main/computercraft/123yeah_boi321/fileSystemManagment/fsmanager.lua")
+    local tempRead = temp.readAll()
+    local fsmanResolved = shell.resolveProgram("fsmanager")
+    local fsmanFile = fs.open(fsmanResolved, "r")
+    local fsman = fsmanFile.readAll()
+    fsmanFile.close()
+    if fsman ~= tempRead then
+        print("It seems that fsmanager may need an update. Would you like to install?")
+        print("Y/N")
+        local re = read()
+        if string.lower(re) == "y" or string.lower(re) == "yes" or string.lower(re) == "update" or string.upper(re) == "HEC YEA BROTHER" then
+            local fsmanFile = fs.open(fsmanResolved,"w")
+            fsmanFile.write(tempRead)
+            fsmanFile.close()
+        end
+    else
+        fs.delete("temp.lua")
+    end
+    term.clear()
 else
-	print("Hmm.. It seems you do not have the main file. Would you like me to download it?")
-	print("Y/N")
-	local re = read()
-	if string.lower(re) == "y" or string.lower(re) == "yes" or string.upper(re) == "HEK YEA BROTHER" then
-		shell.run("wget https://raw.githubusercontent.com/Dead-Knuckle/Minecraft-Computer-Mod-Codes/main/computercraft/123yeah_boi321/fileSystemManagment/fsmanager.lua")
-	else
-		return
-	end
+    print("Hmm.. It seems you do not have the main file. Would you like me to download it?")
+    print("Y/N")
+    local re = read()
+    if string.lower(re) == "y" or string.lower(re) == "yes" or string.upper(re) == "HEK YEA BROTHER" then
+        local temp = http.get("https://raw.githubusercontent.com/YeahBoi321/Minecraft-Computer-Mod-Codes/main/computercraft/123yeah_boi321/fileSystemManagment/fsmanager.lua")
+        local tempRead = temp.readAll()
+        local fsman = fs.open("fsmanager.lua","w")
+        fsman.write(tempRead)
+        fsman.close()
+    else
+    return
+    end
 end
 term.clear()
 local fsm = require(string.gsub(shell.resolveProgram("fsmanager"),".lua",""))
 local dB,dT,dF,dS =fsm.main(false,"","",0)
 term.clear()
 while true do
-	local dBt,dTt,dFt,dSt = fsm.main(dB,dT,dF,dS)
-	dB = dBt
-	dT = dTt
-	dF = dFt
-	dS = dSt
+    local dBt,dTt,dFt,dSt = fsm.main(dB,dT,dF,dS)
+    dB = dBt
+    dT = dTt
+    dF = dFt
+    dS = dSt
 end
