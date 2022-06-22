1224bddedec308c090fe249b1e9112f62ecfbeba
Rewired <97414090+Dead-Knuckle@users.noreply.github.com>
2022-06-22 16:22:32 -0400

    Update TunnelMine.lua

--- computercraft/dead_knuckle/TunnelMine.lua
+++ computercraft/dead_knuckle/TunnelMine.lua
@@ -1,107 +1,127 @@
 --This program is for minning
 --It can use ender chest or shulkerboxes for extra storgae, recommended that you use ender chest or something simlar 
+-- It can also burn items in the blacklist
 -- It mines out tunnels, 30 long it will use torches, use coal as fuel for best results 
 --Made by: Dead_Knuckle
 function start()
-  print("Please Place Torches in Slot 1")
-  print("Please Place Fuel in Slot 16")
-  sleep(2)
-  term.clear()
-  term.setCursorPos(1,1)
-  print("How many Tunnels")
-  x = read()
-   tunnelAmount =x 
-  term.clear()
-  term.setCursorPos(1,1)
-  print("Storage? (y/n)")
-   s =read() 
-   if s == "y" then
-    print("Please Place In Slot 15")
+    print("Please Place Torches in Slot 1")
+    print("Please Place Fuel in Slot 16")
     sleep(2)
-   end  
     term.clear()
-end
-function echest()
-  if s == "y" then
-    turtle.select(15)
-  turtle.placeUp()
-   for i = 2,14 do
-   turtle.select(i)
-   turtle.dropUp()
-   end
-   turtle.select(15)
-   turtle.digUp()
-  turtle.select(1)
+    term.setCursorPos(1,1)
+    print("How many Tunnels")
+    x = read()
+     tunnelAmount =x 
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Storage? (y/n)")
+     s =read() 
+     if s == "y" then
+      print("Please Place In Slot 15")
+      sleep(2)
+     end  
+     print("Void items?")
+      term.clear()
+      v=read()
+      if v == "y" then
+        print("Please place Lava Bucket or Flint and Steel in slot 14")
+        sleep(2)
+        burn = true
+      end
   end
-  end
-function refuel()
- term.setCursorPos(1,1)
- print("Fuel:"..turtle.getFuelLevel())
- if turtle.getFuelLevel() < 100 then
- turtle.select(16)
- turtle.refuel(1)
- turtle.select(1)
- end
-end
-function invSort()   
-  turtle.select(1)
-blackList = {"minecraft:stone","minecraft:cobblestone","minecraft:dirt","minecraft:granite","minecraft:diorite","minecraft:gravel","minecraft:flint", "minecraft:netherrack", "minecraft:cobbled_deepslate", "minecraft:tuff"}
-    for i = 1,15 do
-turtle.select(i)
-x,y,z = turtle.getItemDetail()
- if turtle.getItemCount() < 1 then
- sleep(0.01)
- else
- for i,v in ipairs(blackList) do 
-if x.name == "minecraft:coal" then
-  turtle.transferTo(16)
-end
-if x.name == blackList[i] then
-turtle.dropDown()
-end
- end
+  function echest()
+    if s == "y" then
+      turtle.select(15)
+    turtle.placeUp()
+     for i = 2,14 do
+     turtle.select(i)
+     turtle.dropUp()
+     end
+     turtle.select(15)
+     turtle.digUp()
+    turtle.select(1)
+    end
+    end
+
+function void()
+    if burn then
+    turtle.select(14)
+    turtle.placeDown()
+    sleep(0.2)
+    turtle.placeDown()
+    turtle.down()
+    turtle.up()
     end
 end
-turtle.select(1)
-end
-function mine()
-turtle.dig()
-turtle.forward()
-turtle.digUp()
-turtle.digDown()
-end
-function tunnel()
-turtle.placeDown()
-  for i = 1,3 do
- for i = 1,10 do
- refuel()
- turtle.dig()
- turtle.forward()
- turtle.digUp()
- turtle.digDown()
- end
-  turtle.placeDown() 
+  function refuel()
+   term.setCursorPos(1,1)
+   print("Fuel:"..turtle.getFuelLevel())
+   if turtle.getFuelLevel() < 100 then
+   turtle.select(16)
+   turtle.refuel(1)
+   turtle.select(1)
+   end
   end
-invSort() 
-echest()  
- for i = 1,30 do
- turtle.back()
- end
-end
-function main()
-start()
-  for i = 1,x do
-term.setCursorPos(1,2)
-print("Tunnel:"..i.."/"..tunnelAmount)
-term.setCursorPos(1,1)
-tunnel()
-turtle.turnLeft()
-mine()
-mine()
-mine()
-turtle.turnRight()
-term.clear()
- end
-end
-main()
-echest()
+  function invSort()   
+    turtle.select(1)
+  blackList = {"minecraft:stone","minecraft:cobblestone","minecraft:dirt","minecraft:granite","minecraft:diorite","minecraft:gravel","minecraft:flint", "minecraft:netherrack", "minecraft:cobbled_deepslate", "minecraft:tuff"}
+      for i = 1,15 do
+  turtle.select(i)
+  x,y,z = turtle.getItemDetail()
+   if turtle.getItemCount() < 1 then
+   sleep(0.01)
+   else
+   for i,v in ipairs(blackList) do 
+  if x.name == "minecraft:coal" then
+    turtle.transferTo(16)
+  end
+  if x.name == blackList[i] then
+  turtle.dropDown()
+  end
+   end
+      end
+  end
+  void()
+  turtle.select(1)
+  end
+  function mine()
+  turtle.dig()
+  turtle.forward()
+  turtle.digUp()
+  turtle.digDown()
+  end
+  function tunnel()
+  turtle.placeDown()
+    for i = 1,3 do
+   for i = 1,10 do
+   refuel()
+   turtle.dig()
+   turtle.forward()
+   turtle.digUp()
+   turtle.digDown()
+   end
+    turtle.placeDown() 
+    end
+  invSort() 
+  echest()  
+   for i = 1,30 do
+   turtle.back()
+   end
+  end
+  function main()
+  start()
+    for i = 1,x do
+  term.setCursorPos(1,2)
+  print("Tunnel:"..i.."/"..tunnelAmount)
+  term.setCursorPos(1,1)
+  tunnel()
+  turtle.turnLeft()
+  mine()
+  mine()
+  mine()
+  turtle.turnRight()
+  term.clear()
+   end
+  end
+  main()
+  echest()
