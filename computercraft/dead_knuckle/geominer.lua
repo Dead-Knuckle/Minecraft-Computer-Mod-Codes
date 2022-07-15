18b1936b2bf3734b6f2c8b38a62eb9d3cc99cdf6
Rewired <97414090+Dead-Knuckle@users.noreply.github.com>
2022-07-15 11:52:22 -0400

    Update geominer.lua
    Bug fixes

--- computercraft/dead_knuckle/geominer.lua
+++ computercraft/dead_knuckle/geominer.lua
@@ -2,7 +2,7 @@ local geo = peripheral.find("geoScanner")
 if arg[1] == null then
 whatLooking = "ore"
 else    
-what Looking = tostring(arg[1])
+whatLooking = tostring(arg[1])
 end
 term.clear()
 term.setCursorPos(1,1)
@@ -12,29 +12,35 @@ print("Compass Direction: ")
 local com = read()
 r = tonumber(r)
 print("Put fuel in slot 16")
-function fuelCheck()
-    local f = turtle.getFuelLevel()
-    if f <= r+5 then
-        turtle.select(16)
-        turtle.refuel(64)
+function refuel()
+    term.setCursorPos(1,1)
+    print("Fuel:"..turtle.getFuelLevel())
+    if turtle.getFuelLevel() < 100 then
+    turtle.select(16)
+    turtle.refuel(1)
+    turtle.select(1)
     end
-end
+   end
 
 function main()
     for i = 1,r do
         turtle.dig()
+        refuel()
         turtle.forward()
         turtle.digDown()
     end
     fuelCheck()
     local a = geo.scan(r)
     term.clear()
+    refuel()
+    term.setCursorPos(1,2)
     for i,v in ipairs(a) do
-        if string.find(a[i].name,"ore") then
+        if string.find(a[i].name,whatLooking) then
             print(string.gsub(string.gsub(a[i].name,"minecraft:",""),"_"," ").." is at "..a[i].x.." "..a[i].y.." "..a[i].z)
         end
     end
-    os.pullEvent()
+    print('Please press enter to move forward '.. r .. 'blocks.')
+    read()
 end
 
 while true do
