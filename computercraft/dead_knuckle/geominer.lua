792af71803c5aa8fc9b30a29f3645dcb6828b47b
Rewired <97414090+Dead-Knuckle@users.noreply.github.com>
2022-07-15 17:38:00 -0400

    Update geominer.lua

--- computercraft/dead_knuckle/geominer.lua
+++ computercraft/dead_knuckle/geominer.lua
@@ -1,17 +1,17 @@
 local geo = peripheral.find("geoScanner")
+
 if arg[1] == null then
 whatLooking = "ore"
 else    
 whatLooking = tostring(arg[1])
 end
-term.clear()
-term.setCursorPos(1,1)
 print("Radius: ")
 local r = read()
 print("Compass Direction: ")
 local com = read()
 r = tonumber(r)
 print("Put fuel in slot 16")
+sleep(1)
 term.clear()
 term.setCursorPos(1,1)
 function refuel()
@@ -25,6 +25,10 @@ function refuel()
    end
 
 function main()
+    parrot = {}
+    action = 'boat'
+    term.setCursorPos(1,2)
+    print('Moving...')
     for i = 1,r do
         turtle.dig()
         refuel()
@@ -35,13 +39,29 @@ function main()
     term.clear()
     refuel()
     term.setCursorPos(1,2)
+print('=======================================')
+        term.setCursorPos(1,3)
     for i,v in ipairs(a) do
         if string.find(a[i].name,whatLooking) then
             print(string.gsub(string.gsub(a[i].name,"minecraft:",""),"_"," ").." is at "..a[i].x.." "..a[i].y.." "..a[i].z)
+            table.insert(parrot, a[i].name)
         end
-    end
+print('=======================================')
     print('Please press enter to move forward '.. r .. ' blocks.')
-    read()
+    if parrot[0] == nil 
+    sleep(0.5)
+    else 
+    action = read()
+    end
+    if action == "e" then
+    break
+    end
+    if action == "l" then
+     turtle.turnLeft()
+    end
+    if action == "r" then
+     turtle.turnLeft()
+    end
 end
 
 while true do
